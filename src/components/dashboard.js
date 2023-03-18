3f65319214462b234bc512e0c1c32465a3e0a450
Tun Khine <tunkhine126@gmail.com>
2023-03-18 16:16:05 -0400

    Routes and initial setup

---
+++ src/components/dashboard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+  render() {
+    return(
+      <div>
+        <h1 className="text-3xl font-bold underline text-center">
+          DASHBOARD
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default Dashboard
---
+++ src/components/home.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+  render() {
+    return(
+      <div>
+        <h1 className="text-3xl font-bold underline text-center">
+          Hello world!
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default Home
